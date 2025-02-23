5289df5ba9f7410296a899d8fc9491fc03b423aa
Janetta Doncheva <janettadoncheva@gmail.com>
2025-02-23 14:46:45 +0000

    adding a tasks component which receives the name of the selected user and outputs it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,25 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
     users = DUMMY_USERS;
-
+    selectedUserName = ""
+  
     onSelectUser(id: string) {
-        console.log('Selected User with ID ' + id);;
+       
+        let selectedUser:any = this.users.find ((user) => user.id === id);
+        this.selectedUserName = selectedUser.name
+        
         
     }
+        
 }
